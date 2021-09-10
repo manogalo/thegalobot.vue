d3f6f05a3b4bb303ae1bad5144399773c71a478e
sgnjohnny <johnny@signa.net.br>
2021-09-09 22:22:22 -0300

    feat:get-started page

--- thegalobot/src/router/index.js
+++ thegalobot/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import BotSettings from '../views/BotSettings.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
         path: '/',
         name: 'Home',
         component: Home
+    },
+    {
+        path: '/get-started',
+        name: 'Bot Settings',
+        component: BotSettings
     }
 ]
 
---
+++ thegalobot/src/views/BotSettings.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Bot Settings</h1>
+</template>
