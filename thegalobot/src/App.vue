b8b1389098d560f781d2fd9a76be7b8371e14cf4
sgnjohnny <johnny@signa.net.br>
2021-09-09 22:19:01 -0300

    feat:MrEliasen links on footer

--- thegalobot/src/App.vue
+++ thegalobot/src/App.vue
@@ -11,16 +11,23 @@
         tile
       >
         <v-card-title class="primary">
-          <strong>Get connected with the dev on social networks!</strong>
-          <v-spacer/>
-          <v-btn 
-            icon
-            v-for="item in icons"
-            :key="item.icon"
-            @click="openPage(item.href)"
-          >
-            <v-icon>{{item.icon}}</v-icon>
-          </v-btn>
+          <v-row>
+            <strong>Get connected with the dev on social networks!</strong>
+            <v-spacer/>
+            <v-btn 
+              icon
+              v-for="item in icons"
+              :key="item.icon"
+              @click="openPage(item.href)"
+            >
+              <v-icon>{{item.icon}}</v-icon>
+            </v-btn>
+          </v-row>
+          <v-row>
+            <div>
+              The bot list used by TheGaloBot is kindly provided by <a href="https://markeliasen.com/" style="color:white">Mark Eliasen</a>. Please consider <a href="https://github.com/MrEliasen/twitch-bot-list/blob/master/.github/CONTRIBUTING.md" style="color:white">contributing</a> to the list.
+            </div>
+          </v-row>
         </v-card-title>
 
         <v-card-text class="primary py-2 text-center">
